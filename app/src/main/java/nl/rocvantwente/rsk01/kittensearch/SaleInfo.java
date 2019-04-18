f5cedef570fa862019bf2fa2aeec8d94487d5db3
mgroesink <mroesink@rocvantwente.nl>
2019-04-18 09:51:38 +0200

    First commit

---
+++ app/src/main/java/nl/rocvantwente/rsk01/kittensearch/SaleInfo.java
@@ -0,0 +1,13 @@
+package nl.rocvantwente.rsk01.kittensearch;
+
+public class SaleInfo {
+    private String mBuyLink;
+
+    public String getmBuyLink() {
+        return mBuyLink;
+    }
+
+    public void setmBuyLink(String mBuyLink) {
+        this.mBuyLink = mBuyLink;
+    }
+}
---
+++ app/src/main/java/nl/rocvantwente/rsk01/kittensearch/VolumeInfo.java
@@ -0,0 +1,89 @@
+package nl.rocvantwente.rsk01.kittensearch;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class VolumeInfo {
+    private String mSubtitle;
+    private String mSmallThumbnail;
+    private String mThumbnail;
+    private String mPublisher;
+    private List<String> mAuthors = new ArrayList<String>();
+    private Date mPublishedDate;
+    private int mPageCount;
+    private String mTitle;
+    private String mDescription;
+
+    public Date getmPublishedDate() {
+        return mPublishedDate;
+    }
+
+    public void setmPublishedDate(Date mPublishedDate) {
+        this.mPublishedDate = mPublishedDate;
+    }
+
+    public List<String> getmAuthors() {
+        return mAuthors;
+    }
+
+    public void addAuthor(String name) {
+        mAuthors.add(name);
+    }
+
+    public String getmPublisher() {
+        return mPublisher;
+    }
+
+    public void setmPublisher(String mPublisher) {
+        this.mPublisher = mPublisher;
+    }
+
+    public String getmSubtitle() {
+        return mSubtitle;
+    }
+
+    public void setmSubtitle(String mSubtitle) {
+        this.mSubtitle = mSubtitle;
+    }
+
+    public String getmSmallThumbnail() {
+        return mSmallThumbnail;
+    }
+
+    public void setmSmallThumbnail(String mSmallThumbnail) {
+        this.mSmallThumbnail = mSmallThumbnail;
+    }
+
+    public String getmThumbnail() {
+        return mThumbnail;
+    }
+
+    public void setmThumbnail(String mThumbnail) {
+        this.mThumbnail = mThumbnail;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public int getmPageCount() {
+        return mPageCount;
+    }
+
+    public void setmPageCount(int mPageCount) {
+        this.mPageCount = mPageCount;
+    }
+}
